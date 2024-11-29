2e41e1fbeb774a25726a6c41380d89f1945bfff2
PraveenSingh1997 <praveen.singh@csu.fullerton.edu>
2024-11-29 21:51:47 +0530

    first commit 1

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+# Step 1: Initialize a Spark session
+spark = SparkSession.builder \
+    .appName("PySpark Starter Template") \
+    .master("local[*]") \
+    .getOrCreate()
+
+
+input_path = "C:\Miora Projects\order_region_a(in).csv"  # Update with your file path
+df = spark.read.option("header", "true").option("inferSchema", "true").csv(input_path)
+
+
+print(df.head())
